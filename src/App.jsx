0ef4b976ee961be0728533137187f829f3fb4a91
siraz <siraz@gmail.com>
2023-06-04 20:07:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './components/styles.scss'
+import Board from './components/Board'
+
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <Board/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Square({value}) {
+  return (
+    <>
+    <button type="button" className="square">{value}</button>
+    </>
+  );
+}
+export default Square
